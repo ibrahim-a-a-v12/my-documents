0f385687ccd6e39da7f409f874795ed47163d178
ibrahim-a-a-v12 <141725621+ibrahim-a-a-v12@users.noreply.github.com>
2024-06-28 12:25:35 +0100

    Create checkDuplicateClassifieldsGeneratedFileName.sql

---
+++ checkDuplicateClassifieldsGeneratedFileName.sql
@@ -0,0 +1,16 @@
+SELECT generated_file_name , COUNT(generated_file_name) FROM classifields GROUP BY generated_file_name HAVING COUNT(generated_file_name) > 1;
+
+SELECT * FROM classifields WHERE generated_file_name = "smartchoiceauto";
+
+6031612
+users/DCS.22532/22532
+985398fc8f9a29bb
+74241363
+5378263
+209839
+5396757
+113070
+5378992
+484116.csv
+smartchoiceauto
+
