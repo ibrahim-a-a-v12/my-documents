d86631ea2362846f5598200fa014f439170b15c3
ibrahim-a-a-v12 <141725621+ibrahim-a-a-v12@users.noreply.github.com>
2024-07-03 17:51:04 +0100

    Create SelectQueryToCalculAndSortAllAccountsTables.sql

---
+++ SelectQueryToCalculAndSortAllAccountsTables.sql
@@ -0,0 +1,264 @@
+SELECT 'account_7listings' AS table_name, COUNT(*) AS count FROM account_7listings
+UNION ALL
+SELECT 'account_atclassics', COUNT(*) FROM account_atclassics
+UNION ALL
+SELECT 'account_authenticom', COUNT(*) FROM account_authenticom
+UNION ALL
+SELECT 'account_authenticom_2', COUNT(*) FROM account_authenticom_2
+UNION ALL
+SELECT 'account_authenticom_3', COUNT(*) FROM account_authenticom_3
+UNION ALL
+SELECT 'account_auto2go', COUNT(*) FROM account_auto2go
+UNION ALL
+SELECT 'account_autoclick', COUNT(*) FROM account_autoclick
+UNION ALL
+SELECT 'account_autoconx', COUNT(*) FROM account_autoconx
+UNION ALL
+SELECT 'account_autofusion', COUNT(*) FROM account_autofusion
+UNION ALL
+SELECT 'account_autolist', COUNT(*) FROM account_autolist
+UNION ALL
+SELECT 'account_automanager', COUNT(*) FROM account_automanager
+UNION ALL
+SELECT 'account_autopages123', COUNT(*) FROM account_autopages123
+UNION ALL
+SELECT 'account_autopaymentfinder', COUNT(*) FROM account_autopaymentfinder
+UNION ALL
+SELECT 'account_autorevo', COUNT(*) FROM account_autorevo
+UNION ALL
+SELECT 'account_autorevolution', COUNT(*) FROM account_autorevolution
+UNION ALL
+SELECT 'account_autorv', COUNT(*) FROM account_autorv
+UNION ALL
+SELECT 'account_autotrader', COUNT(*) FROM account_autotrader
+UNION ALL
+SELECT 'account_backpage', COUNT(*) FROM account_backpage
+UNION ALL
+SELECT 'account_bayareadealers', COUNT(*) FROM account_bayareadealers
+UNION ALL
+SELECT 'account_blacklist', COUNT(*) FROM account_blacklist
+UNION ALL
+SELECT 'account_blogger', COUNT(*) FROM account_blogger
+UNION ALL
+SELECT 'account_boostmotors', COUNT(*) FROM account_boostmotors
+UNION ALL
+SELECT 'account_cargigi', COUNT(*) FROM account_cargigi
+UNION ALL
+SELECT 'account_cargurus', COUNT(*) FROM account_cargurus
+UNION ALL
+SELECT 'account_carscom', COUNT(*) FROM account_carscom
+UNION ALL
+SELECT 'account_carsforsale', COUNT(*) FROM account_carsforsale
+UNION ALL
+SELECT 'account_carsoup', COUNT(*) FROM account_carsoup
+UNION ALL
+SELECT 'account_carsource', COUNT(*) FROM account_carsource
+UNION ALL
+SELECT 'account_carweek', COUNT(*) FROM account_carweek
+UNION ALL
+SELECT 'account_cbb', COUNT(*) FROM account_cbb
+UNION ALL
+SELECT 'account_cobalt', COUNT(*) FROM account_cobalt
+UNION ALL
+SELECT 'account_corvetteforum', COUNT(*) FROM account_corvetteforum
+UNION ALL
+SELECT 'account_craigslist', COUNT(*) FROM account_craigslist
+UNION ALL
+SELECT 'account_credit_acceptance', COUNT(*) FROM account_credit_acceptance
+UNION ALL
+SELECT 'account_cudl', COUNT(*) FROM account_cudl
+UNION ALL
+SELECT 'account_dallasautoplace', COUNT(*) FROM account_dallasautoplace
+UNION ALL
+SELECT 'account_dbcl', COUNT(*) FROM account_dbcl
+UNION ALL
+SELECT 'account_dealercarsearch', COUNT(*) FROM account_dealercarsearch
+UNION ALL
+SELECT 'account_dealerclick', COUNT(*) FROM account_dealerclick
+UNION ALL
+SELECT 'account_dealerclick2', COUNT(*) FROM account_dealerclick2
+UNION ALL
+SELECT 'account_dealerfire', COUNT(*) FROM account_dealerfire
+UNION ALL
+SELECT 'account_dealerslink', COUNT(*) FROM account_dealerslink
+UNION ALL
+SELECT 'account_dealerspeedleads', COUNT(*) FROM account_dealerspeedleads
+UNION ALL
+SELECT 'account_delicious', COUNT(*) FROM account_delicious
+UNION ALL
+SELECT 'account_diigo', COUNT(*) FROM account_diigo
+UNION ALL
+SELECT 'account_dmispecial', COUNT(*) FROM account_dmispecial
+UNION ALL
+SELECT 'account_dmotorworks', COUNT(*) FROM account_dmotorworks
+UNION ALL
+SELECT 'account_dodah', COUNT(*) FROM account_dodah
+UNION ALL
+SELECT 'account_drivenloans', COUNT(*) FROM account_drivenloans
+UNION ALL
+SELECT 'account_ebay', COUNT(*) FROM account_ebay
+UNION ALL
+SELECT 'account_ebayclassifieds', COUNT(*) FROM account_ebayclassifieds
+UNION ALL
+SELECT 'account_ebizautos', COUNT(*) FROM account_ebizautos
+UNION ALL
+SELECT 'account_eliteautoimports', COUNT(*) FROM account_eliteautoimports
+UNION ALL
+SELECT 'account_facebook', COUNT(*) FROM account_facebook
+UNION ALL
+SELECT 'account_facebook_pages', COUNT(*) FROM account_facebook_pages
+UNION ALL
+SELECT 'account_fidelityventures', COUNT(*) FROM account_fidelityventures
+UNION ALL
+SELECT 'account_flickr', COUNT(*) FROM account_flickr
+UNION ALL
+SELECT 'account_flipboard', COUNT(*) FROM account_flipboard
+UNION ALL
+SELECT 'account_fmes', COUNT(*) FROM account_fmes
+UNION ALL
+SELECT 'account_fortynineermotors', COUNT(*) FROM account_fortynineermotors
+UNION ALL
+SELECT 'account_freebeeauto', COUNT(*) FROM account_freebeeauto
+UNION ALL
+SELECT 'account_fusionautoplex', COUNT(*) FROM account_fusionautoplex
+UNION ALL
+SELECT 'account_googlebusiness', COUNT(*) FROM account_googlebusiness
+UNION ALL
+SELECT 'account_googlebusinessi', COUNT(*) FROM account_googlebusinessi
+UNION ALL
+SELECT 'account_googlemybusiness_locations', COUNT(*) FROM account_googlemybusiness_locations
+UNION ALL
+SELECT 'account_googleplus', COUNT(*) FROM account_googleplus
+UNION ALL
+SELECT 'account_google_analytic', COUNT(*) FROM account_google_analytic
+UNION ALL
+SELECT 'account_homenet', COUNT(*) FROM account_homenet
+UNION ALL
+SELECT 'account_imagiclab', COUNT(*) FROM account_imagiclab
+UNION ALL
+SELECT 'account_infinitypublishing', COUNT(*) FROM account_infinitypublishing
+UNION ALL
+SELECT 'account_instagram', COUNT(*) FROM account_instagram
+UNION ALL
+SELECT 'account_instapaper', COUNT(*) FROM account_instapaper
+UNION ALL
+SELECT 'account_iocom', COUNT(*) FROM account_iocom
+UNION ALL
+SELECT 'account_ironhorse', COUNT(*) FROM account_ironhorse
+UNION ALL
+SELECT 'account_islandcarbuyer', COUNT(*) FROM account_islandcarbuyer
+UNION ALL
+SELECT 'account_karklix', COUNT(*) FROM account_karklix
+UNION ALL
+SELECT 'account_keepitlocal', COUNT(*) FROM account_keepitlocal
+UNION ALL
+SELECT 'account_kgi', COUNT(*) FROM account_kgi
+UNION ALL
+SELECT 'account_kijiji', COUNT(*) FROM account_kijiji
+UNION ALL
+SELECT 'account_letgo', COUNT(*) FROM account_letgo
+UNION ALL
+SELECT 'account_letgo_existing', COUNT(*) FROM account_letgo_existing
+UNION ALL
+SELECT 'account_linkedin', COUNT(*) FROM account_linkedin
+UNION ALL
+SELECT 'account_livejournal', COUNT(*) FROM account_livejournal
+UNION ALL
+SELECT 'account_merchenta', COUNT(*) FROM account_merchenta
+UNION ALL
+SELECT 'account_mojo', COUNT(*) FROM account_mojo
+UNION ALL
+SELECT 'account_mywurld', COUNT(*) FROM account_mywurld
+UNION ALL
+SELECT 'account_olx', COUNT(*) FROM account_olx
+UNION ALL
+SELECT 'account_openlane', COUNT(*) FROM account_openlane
+UNION ALL
+SELECT 'account_ove', COUNT(*) FROM account_ove
+UNION ALL
+SELECT 'account_photobucket', COUNT(*) FROM account_photobucket
+UNION ALL
+SELECT 'account_picasa', COUNT(*) FROM account_picasa
+UNION ALL
+SELECT 'account_pinterest', COUNT(*) FROM account_pinterest
+UNION ALL
+SELECT 'account_pinterest_boards', COUNT(*) FROM account_pinterest_boards
+UNION ALL
+SELECT 'account_plurk', COUNT(*) FROM account_plurk
+UNION ALL
+SELECT 'account_prad', COUNT(*) FROM account_prad
+UNION ALL
+SELECT 'account_printed_forms', COUNT(*) FROM account_printed_forms
+UNION ALL
+SELECT 'account_recycler', COUNT(*) FROM account_recycler
+UNION ALL
+SELECT 'account_reddit', COUNT(*) FROM account_reddit
+UNION ALL
+SELECT 'account_roncraftchevrolet', COUNT(*) FROM account_roncraftchevrolet
+UNION ALL
+SELECT 'account_rvdownload', COUNT(*) FROM account_rvdownload
+UNION ALL
+SELECT 'account_rvregistry', COUNT(*) FROM account_rvregistry
+UNION ALL
+SELECT 'account_rvtrader', COUNT(*) FROM account_rvtrader
+UNION ALL
+SELECT 'account_rvusa', COUNT(*) FROM account_rvusa
+UNION ALL
+SELECT 'account_smartauction', COUNT(*) FROM account_smartauction
+UNION ALL
+SELECT 'account_smartpillars', COUNT(*) FROM account_smartpillars
+UNION ALL
+SELECT 'account_smokinvette', COUNT(*) FROM account_smokinvette
+UNION ALL
+SELECT 'account_specialties', COUNT(*) FROM account_specialties
+UNION ALL
+SELECT 'account_speech_recognition', COUNT(*) FROM account_speech_recognition
+UNION ALL
+SELECT 'account_stumbleupon', COUNT(*) FROM account_stumbleupon
+UNION ALL
+SELECT 'account_sunsetmotors', COUNT(*) FROM account_sunsetmotors
+UNION ALL
+SELECT 'account_targetmediapartners', COUNT(*) FROM account_targetmediapartners
+UNION ALL
+SELECT 'account_tentoone', COUNT(*) FROM account_tentoone
+UNION ALL
+SELECT 'account_texasdrive', COUNT(*) FROM account_texasdrive
+UNION ALL
+SELECT 'account_totrader', COUNT(*) FROM account_totrader
+UNION ALL
+SELECT 'account_traderca', COUNT(*) FROM account_traderca
+UNION ALL
+SELECT 'account_tradercaas', COUNT(*) FROM account_tradercaas
+UNION ALL
+SELECT 'account_tradercasp', COUNT(*) FROM account_tradercasp
+UNION ALL
+SELECT 'account_tradercatrucks', COUNT(*) FROM account_tradercatrucks
+UNION ALL
+SELECT 'account_travidia', COUNT(*) FROM account_travidia
+UNION ALL
+SELECT 'account_truecar', COUNT(*) FROM account_truecar
+UNION ALL
+SELECT 'account_tumblr', COUNT(*) FROM account_tumblr
+UNION ALL
+SELECT 'account_twitter', COUNT(*) FROM account_twitter
+UNION ALL
+SELECT 'account_untangler', COUNT(*) FROM account_untangler
+UNION ALL
+SELECT 'account_usedcorvettes', COUNT(*) FROM account_usedcorvettes
+UNION ALL
+SELECT 'account_v12tov12', COUNT(*) FROM account_v12tov12
+UNION ALL
+SELECT 'account_vast', COUNT(*) FROM account_vast
+UNION ALL
+SELECT 'account_vbulletin', COUNT(*) FROM account_vbulletin
+UNION ALL
+SELECT 'account_vehix', COUNT(*) FROM account_vehix
+UNION ALL
+SELECT 'account_vground', COUNT(*) FROM account_vground
+UNION ALL
+SELECT 'account_vkontakte', COUNT(*) FROM account_vkontakte
+UNION ALL
+SELECT 'account_wordpress', COUNT(*) FROM account_wordpress
+UNION ALL
+SELECT 'account_xing', COUNT(*) FROM account_xing
+ORDER BY count DESC;
